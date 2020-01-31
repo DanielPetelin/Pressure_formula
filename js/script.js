6868b304a1641fa2857f58dbce0492edd7b018f4
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-31 20:44:59 +0300

    add translate

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+document.getElementById('translate').onclick = function() {
+    var smk = document.getElementById('smk').value;
+    var mk = 10000;
+
+    document.getElementById('outm').innerHTML = smk / mk;
+};
+
 document.getElementById('count').onclick = function() {
     var ves = document.getElementById('ves').value;
     var ploshad = document.getElementById('ploshad').value;
